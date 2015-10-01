690155367b1bdb22dc0767402811a0671b69553f
ArminSebastian <arminsebastian@gmail.com>
2015-10-01 13:50:05 -0700

    feat(requester_task): start setup for component

--- client/src/components/requester_task/requesterTaskCtrl.js
+++ client/src/components/requester_task/requesterTaskCtrl.js
@@ -2,9 +2,17 @@
   'use strict';
 
   angular.module('app.requester_task', [])
-  .controller('RequesterTaskCtrl', [function() {
-    var vm = this;
+  .controller('RequesterTaskCtrl', ['profileFactory', function(profileFactory) {
 
+    var vm = this;
+    //assumes that the object has a location property
+    vm.userProfile = profileFactory.getProfile();
+
+    vm.getMap = function (location){
+      return userProfile.location;
+    };
+
+    vm.getMap(userProfile.location);
 
   }]);
 
