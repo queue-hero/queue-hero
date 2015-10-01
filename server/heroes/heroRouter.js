8c782daca203ea38387138e1b581d604466bf5a8
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-09-30 17:40:32 -0700

    style(server routing): add comment to heroRouter, requesterRouter

--- server/heroes/heroRouter.js
+++ server/heroes/heroRouter.js
@@ -1,11 +1,29 @@
 var heroCtrl = require('./heroCtrl.js');
 
 module.exports = function (app) {
-  // need to add specific method to call for each route
+  // Still need to add specific method to call for each route
   // app.get('/location', heroCtrl.doThis())
+
+  // req: browser nav. location
+  // res: map and location options
   app.get('/location');
+  // req: option chosen / location of hero
+  // res: 201 or error
   app.post('/location');
+
+  // req: browser nav. location
+  // map and location options
   app.get('/task');
+  // req: obj with order details from the controller
+  // res: 201 or error
   app.post('/task');
+
+  // req: Polling
+  // res: Polling
+  app.post('/order');
+
+  // Need to resolve how to direct two different POSTS from same url
+  // req: order received + rating, queuehero id and requester id
+  // res: 201 or error
   app.post('/order');
 };
--- server/requesters/requesterRouter.js
+++ server/requesters/requesterRouter.js
@@ -1,10 +1,18 @@
 var requesterCtrl = require('./requesterCtrl.js');
 
 module.exports = function (app) {
-  // need to add specific method to call for each route
+
+  // Still need to add specific method to call for each route
   // app.get('/location', requesterCtrl.doThis())
+
+  // req: nav. location
+  // res: map and location options)
   app.get('/task');
+  // req: obj with order details
+  // res: 201 or error
   app.post('/task');
-  app.get('/order');
+
+  // req: order received + rating, queueheroId and requesterId
+  // res: 201 or error
   app.post('/order');
 };
