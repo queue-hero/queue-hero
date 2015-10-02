3d936c91b191ecbb84652898f2dd026cd2ba8a45
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-10-02 10:54:28 -0700

    fix(transactionModel): set all values to not required

--- server/transactions/transactionModel.js
+++ server/transactions/transactionModel.js
@@ -3,36 +3,28 @@ var mongoose = require('mongoose');
 var TransactionSchema = new mongoose.Schema({
   queueHero: {
     type: String,
-    required: true
   },
   requester: {
     type: String,
-    required: true
   },
   item: {
     type: String,
-    required: true
   },
   moneyExchanged: {
     type: Number,
-    required: true
   },
   meetingTime: {
     type: Date,
-    required: true
   },
   //[lat, long]
   meetingLocation: {
     type: Array,
-    required: true
   },
   vendor: {
     type: String,
-    required: true
   },
   status: {
     type: String,
-    required: true
   },
 });
 
