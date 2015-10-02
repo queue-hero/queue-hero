576a5dbb3baf6a69393c8869dd1abc1d0db2aeae
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-10-02 15:28:20 -0700

    feat(serverChoice): return user data on GET request

---
+++ server/users/userCtrl.js
@@ -0,0 +1,17 @@
+var User = require('./userModel.js');
+var Q = require('q');
+
+module.exports = {
+  getUserData: function(req, res, next) {
+    var username = req.body.username;
+    User.findOne({
+      username: username
+    }, function(err, user) {
+      if (err) {
+        return next(err);
+      }
+      res.status(201).send(user);
+    });
+
+  }
+};
