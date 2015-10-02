2c2642696763b83b849f787c8eca01581c29aa5f
Tatsumi Tagawa <tatsumi.tagawa@gmail.com>
2015-10-01 19:52:11 -0700

    fix(userModel): move paymentSchema properties into userSchema

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -2,52 +2,44 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
   username: {
-    type: String, 
-    required: true, 
+    type: String,
+    required: true,
     unique: true
-  },    
+  },
   password: {
-    type: String, 
+    type: String,
     required: true
-  }, 
+  },
   profilePhoto: {
-    type: String, 
+    type: String,
     required: true,
     default: 'placeholder/image'
-  }, 
+  },
   firstName: {
-    type: String, 
+    type: String,
     required: true
-  }, 
+  },
   lastName: {
-    type: String, 
+    type: String,
     required: true
   },
   phoneNumber: {
-    type: Number, 
+    type: Number,
     required: true
-  }, 
-  paymentDetails: [PaymentDetailSchema],
-  address: {
-    type: String, 
-    required: true
-  }
-});
-
-var PaymentDetailSchema = new mongoose.Schema({
+  },
   cardNumber: {
-    type: Number, 
+    type: Number,
     required: true,
     unique: true,
   },
   cvc: {
-    type: Number, 
+    type: Number,
     required: true,
   },
   expirationDate: {
-    type: String, 
+    type: String,
     required: true,
-  }, 
+  }
 });
 
-module.exports = mongoose.model('users', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
