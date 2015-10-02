f0d874561ccaeed8de4c76672e0b1bad5967aac5
shreeya <shreeya.goel@gmail.com>
2015-10-01 19:08:55 -0700

    feat(client): Create booleans for showing/hiding views

--- client/src/components/requester_task/requesterTaskCtrl.js
+++ client/src/components/requester_task/requesterTaskCtrl.js
@@ -4,6 +4,11 @@
   angular.module('app.requester_task', [])
   .controller('RequesterTaskCtrl', [function() {
     var vm = this;
+    
+    vm.location = true;
+    vm.item = false;
+    vm.timePrice = false;
+    vm.confirm = false;
 
 
   }]);
