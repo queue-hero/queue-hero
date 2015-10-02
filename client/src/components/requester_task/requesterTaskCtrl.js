3622e01c273837fb10b4d900402345f82bedcc57
shreeya <shreeya.goel@gmail.com>
2015-10-01 19:28:03 -0700

    feat(client): Create controller method for sending order
    Add ajaxFactory to send order info to server

--- client/src/components/requester_task/requesterTaskCtrl.js
+++ client/src/components/requester_task/requesterTaskCtrl.js
@@ -4,11 +4,30 @@
   angular.module('app.requester_task', [])
   .controller('RequesterTaskCtrl', [function() {
     var vm = this;
-    
+
     vm.location = true;
     vm.item = false;
     vm.timePrice = false;
     vm.confirm = false;
+
+    vm.confirmOrder = function() {
+      //TODO: get all order details from factory
+      
+      //make ajaxFactoryRequest
+      //FIX: Hardcoded order for now
+      ajaxFactoryRequest.sendOrder({ item: 'Starbucks Frappucino', 
+                                     price: 6, 
+                                     time: Date.now() })
+        .then(function (response) {
+
+          console.log('order was submitted successfully');
+
+          //move to next state
+          $state.go('requester_order');
+        }, function (response) {
+          console.log(response.status);
+        })
+    };
 
 
   }]);
