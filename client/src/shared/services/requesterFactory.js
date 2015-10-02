09bf4c5af0e7ec22ee40474b5dbb1477d4cb2776
darrinmn9 <dnagengast9@gmail.com>
2015-10-02 15:24:29 -0700

    feat(dataFactories) add undefined data objects in factories

--- client/src/shared/services/requesterFactory.js
+++ client/src/shared/services/requesterFactory.js
@@ -2,56 +2,66 @@
   'use strict';
 
   angular.module('app')
-  .factory('requesterFactory', [function() {
-    var order = {};
+    .factory('requesterFactory', [function() {
+      var order = {
+        transactionId: undefined,
+        queueHero: undefined,
+        requester: undefined,
+        item: undefined,
+        moneyExchanged: undefined,
+        vendor: undefined,
+        meetingLocation: undefined,
+        meetingTime: undefined,
+        status: undefined
+      };
 
-    return {
-      getOrder: getOrder,
-      setOrder: setOrder
-    };
+      return {
+        getOrder: getOrder,
+        setOrder: setOrder
+      };
 
-    function getOrder(keys) {
-      if(keys === undefined){
-        return order;
-      }
+      function getOrder(keys) {
+        if (keys === undefined) {
+          return order;
+        }
 
-      if(angular.isArray(keys)){
-        var results = {};
-        for(var i = 0; i < keys.length; i++){
-          if(keys[i] in order){
-            results[keys[i]] = order[keys[i]];
-          }else{
-            results[keys[i]] = null;
+        if (angular.isArray(keys)) {
+          var results = {};
+          for (var i = 0; i < keys.length; i++) {
+            if (keys[i] in order) {
+              results[keys[i]] = order[keys[i]];
+            } else {
+              results[keys[i]] = null;
+            }
+          }
+          return results;
+        }
+
+        if (angular.isString(keys)) {
+          if (keys in order) {
+            return order[keys];
           }
         }
-        return results;
-      }
 
-      if(angular.isString(keys)){
-        if(keys in order){
-          return order[keys];
-        }
-      }
-
-      return null;
-    }
-
-
-    function setOrder(obj) {
-      if(!angular.isObject(obj)){
         return null;
       }
-      var allValidKeys = true;
-      for(var key in obj){
-        if(key in order){
-          order[key] = obj[key];
-        }else{
-          allValidKeys = false;
-        }
-      }
-      return allValidKeys;
-    }
 
-  }]);
+
+      function setOrder(obj) {
+        if (!angular.isObject(obj)) {
+          return null;
+        }
+        var allValidKeys = true;
+        for (var key in obj) {
+          if (key in order) {
+            order[key] = obj[key];
+          } else {
+            allValidKeys = false;
+          }
+        }
+        return allValidKeys;
+      }
+
+    }]);
 
 })();
