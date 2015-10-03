921c2589dccec63d9980e5ff61f54302e61e8896
darrinmn9 <dnagengast9@gmail.com>
2015-10-03 16:41:35 -0700

    fix(redirect): removes redirect from non authenticated users

--- server/users/userCtrl.js
+++ server/users/userCtrl.js
@@ -2,6 +2,7 @@ var User = require('./userModel.js');
 
 module.exports = {
   getUserData: function(req, res, next) {
+    console.log('here');
     var username = req.query.username;
     User.findOne({
       username: username
