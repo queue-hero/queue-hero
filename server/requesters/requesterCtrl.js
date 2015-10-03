61766e6d7256f9afd8e19a1c33e2364627e1b8d6
shreeya <shreeya.goel@gmail.com>
2015-10-03 11:24:02 -0700

    fix(requester/task): refactor post req handler for confirming order

--- server/requesters/requesterCtrl.js
+++ server/requesters/requesterCtrl.js
@@ -4,30 +4,39 @@ var Q = require('q');
 module.exports = {
   createTransaction: function(req, res, next) {
     
-    //extract order from req
+    //extract order details from req
+    var requester = req.body.order.requester;
     var item = req.body.order.item;
-    var time = req.body.order.time;
-    var price = req.body.order.price;
+    var moneyExchanged = req.body.order.moneyExchanged;
+    var vendor = req.body.order.vendor;
+    var meetingLocation = req.body.order.meetingLocation;
+    var meetingTime = req.body.order.meetingTime;
+    var status = 'unfulfilled';
 
-    res.writeHead(201);
-    res.end();
+    //TODO: (db) insert a new transaction with order details into transactions
 
-    //TODO: insert a new transaction with order details into db
+    //FIX: send transaction id of newly created transaction to client
+    console.log('Transaction was created!');
+    res.sendStatus(201).send(1);
   }, 
   fulfillTransaction: function(req, res, next) {
+
     //extract transaction id from req
     var transactionId = req.body.transactionId;
 
-    //TODO: update the transaction status of above transaction
+    //TODO: (db) update the transaction status of above transaction to 'fulfilled'
 
-    res.status(201).send('Transaction was fulfilled!'); 
+    console.log('Transaction was fulfilled!')
+    res.status(201).send('Transaction fulfilled!'); 
   }, 
   checkOrderAccepted: function(req, res, next) {
     //extract transaction id from req
     var transactionId = req.body.transactionId;
 
-    //TODO: check the status of above transaction, return 
-    //true if status === 'fulfilled', false otherwise.
+    //TODO: (db) check the status of above transaction, return 
+    //true if status === 'accepted', false otherwise.
+
+    //FIX: change response status to be whatever the db says
     console.log('Transaction was accepted by a queue hero!');
     res.status(201).send(true);
   }
