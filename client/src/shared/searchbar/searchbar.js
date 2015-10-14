42ea914ac7bcde9a97ee689e8002ffaa290e88d0
darrinmn9 <dnagengast9@gmail.com>
2015-10-13 23:50:18 -0700

    style(searchbar): modify styling to confirm to guide

--- client/src/shared/searchbar/searchbar.js
+++ client/src/shared/searchbar/searchbar.js
@@ -14,37 +14,35 @@
         link: function(scope, element, attrs, model) {
 
           //options for autocomplete
-          var opts;
+          var myOptions;
 
-          //convert options provided to opts
-          var initOpts = function() {
-            opts = {};
+          //convert options provided to myOptions
+          var initOptions = function() {
+            myOptions = {};
             if (scope.options) {
               if (scope.options.types) {
-                opts.types = [];
-                opts.types.push(scope.options.types);
+                myOptions.types = [];
+                myOptions.types.push(scope.options.types);
               }
               if (scope.options.bounds) {
-                opts.bounds = scope.options.bounds;
+                myOptions.bounds = scope.options.bounds;
               }
               if (scope.options.country) {
-                opts.componentRestrictions = {
+                myOptions.componentRestrictions = {
                   country: scope.options.country
                 };
               }
             }
           };
-          initOpts();
+          initOptions();
 
           //create new autocomplete
           //reinitializes on every change of the options provided
           var newAutocomplete = function() {
-            scope.gPlace = new google.maps.places.Autocomplete(element[0], opts);
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], myOptions);
             google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
               scope.$apply(function() {
-                //              if (scope.details) {
                 scope.details = scope.gPlace.getPlace();
-                //              }
                 scope.ngAutocomplete = element.val();
               });
             });
@@ -55,8 +53,9 @@
           scope.watchOptions = function() {
             return scope.options;
           };
+
           scope.$watch(scope.watchOptions, function() {
-            initOpts();
+            initOptions();
             newAutocomplete();
             element[0].value = '';
             scope.ngAutocomplete = element.val();
