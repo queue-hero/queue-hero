5e727fc44bb140b8d9f57058b0ff99587a3f1a3a
darrinmn9 <dnagengast9@gmail.com>
2015-10-15 00:29:10 -0700

    feat(heroTaskCtrl): orders update view websockets

--- server/socket_server.js
+++ server/socket_server.js
@@ -5,7 +5,7 @@ module.exports = function(server) {
 
   var path = require('path');
   var io = require('socket.io')(server);
-  var users = {};
+  // var users = {};
 
   io.on('connection', function(socket) {
     console.log('socket connected');
@@ -13,15 +13,24 @@ module.exports = function(server) {
     //   users[data] = socket;
     // });
 
+    //from requesterTaskCtrl
     socket.on('getHeroCount', function(yelpId) {
       getOpenHeroCount(yelpId, function(num) {
         socket.emit('newHeroCount', [yelpId, num]);
       });
     });
 
+    //from heroLocationCtrl
     socket.on('getRequestCount', function(yelpId) {
       getOpenRequestCount(yelpId, function(num) {
         socket.emit('newRequestCount', [yelpId, num]);
+      });
+    });
+
+    //from heroTaskCtrl
+    socket.on('getOpenRequests', function(yelpId) {
+      getOpenRequests(yelpId, function(transactions) {
+        socket.emit('newOpenRequests', transactions);
       });
     });
 
@@ -32,7 +41,7 @@ module.exports = function(server) {
 
   });
 
-
+  //from requesterTaskCtrl
   function getOpenHeroCount(yelpId, callback) {
     Checkin.count({
       vendorYelpId: yelpId
@@ -41,13 +50,29 @@ module.exports = function(server) {
     });
   }
 
+  //from heroLocationCtrl
   function getOpenRequestCount(yelpId, callback) {
     Transaction.count({
       vendorYelpId: yelpId,
       status: 'unfulfilled',
-      meetingTime: { $gte: Date.now() }
+      meetingTime: {
+        $gte: Date.now()
+      }
     }, function(err, num) {
       callback(num);
+    });
+  }
+
+  //from heroTaskCtrl
+  function getOpenRequests(yelpId, callback) {
+    Transaction.find({
+      status: 'unfulfilled',
+      vendorYelpId: yelpId,
+      meetingTime: {
+        $gte: Date.now()
+      }
+    }, function(err, transactions) {
+      callback(transactions);
     });
   }
 
